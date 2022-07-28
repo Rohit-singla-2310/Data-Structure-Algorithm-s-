64e61fcf8799c615833fc442433dfb4b9074000f
Rohit Singla <74494475+Rohit-singla-2310@users.noreply.github.com>
2022-07-29 01:59:30 +0530

    Update Unique_Element.cpp

--- Array/Unique_Element.cpp
+++ Array/Unique_Element.cpp
@@ -3,7 +3,7 @@ Design the logic to remove the duplicate elements from an
 Array and after the deletion the array should contain the unique 
 elements.
 
-Made By : Gaurav baweja
+Made By : Rohit Singla
 */
 #include<iostream>
 using namespace std;
